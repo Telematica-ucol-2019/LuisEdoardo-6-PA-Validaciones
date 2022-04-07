5b5c3b41fc128df47f54f290badba14027d4ac7b
Luis Leyva <lmorales6@ucol.mx>
2022-04-06 22:38:21 -0500

    Add project files.

---
+++ Formulario/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Formulario.View;
+
+namespace Formulario
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new FormularioPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Formulario/ViewModel/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formulario.ViewModel
+{
+    internal class BaseViewModel
+    {
+    }
+}
